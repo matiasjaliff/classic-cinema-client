cb61a94045029aae986e0d2971e85f4b9b770f4f
Matias Jaliff <matiasjaliff@gmail.com>
2022-08-29 20:39:22 -0300

    started: navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="bg-zinc-800 text-white">
+      <div id="navbar">
+        <Navbar />
+      </div>
+      <div id="content">Content</div>
+      <div id="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
